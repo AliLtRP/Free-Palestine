018d2b1de9f5e92779363e95d2640a38499b29d0
Ali Ahmed <albaghdadyali1@gmail.com>
2023-11-02 21:11:33 +0300

    navbar done

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <body>
+    <NavBar />
+    <HomeComp />
+
+  </body>
+</template>
+
+
+<script>
+import HomeComp from './components/home/HomeComp.vue'
+import NavBar from './components/home/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomeComp,
+    NavBar
+  }
+}
+
+</script>
+
+
+<style src="./assets/tailwind.css"/>
---
+++ src/components/home/NavBar.vue
@@ -0,0 +1,72 @@
+<template>
+    <header>
+        <nav class="nav">
+            <div class="logo">
+                <img v-bind:src="logo" class="logo-img" alt="palestine logo">
+            </div>
+
+            <div class="side-nav">
+                <a href="about page">about</a>
+                <a href="github page of the creater">github</a>
+            </div>
+        </nav>
+    </header>
+</template>
+
+
+<script>
+import logo from '../../assets/palestineLow-2.svg'
+
+
+export default {
+    name: "logoImage",
+    data() {
+        return {
+            logo
+        }
+    }
+}
+</script>
+
+
+<style>
+body {
+    width: 100vw;
+    height: 100vh;
+    background: rgb(0, 17, 36);
+    background: linear-gradient(0deg, rgba(0, 17, 36, 1) 0%, rgba(71, 59, 240, 1) 44%);
+}
+
+header {
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+}
+
+.nav {
+    height: inherit;
+    width: 80%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.logo {
+    width: 30px;
+    height: 70px;
+}
+
+.side-nav {
+    width: 150px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.side-nav a {
+    text-decoration: none;
+    color: white;
+}
+
+</style>
