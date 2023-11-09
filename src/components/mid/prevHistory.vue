6fc3ef07ee7035fdc8a21ba78c1aba8b661044a6
Ali Ahmed <albaghdadyali1@gmail.com>
2023-11-09 22:33:45 +0300

    add vr line content

--- src/components/mid/prevHistory.vue
+++ src/components/mid/prevHistory.vue
@@ -1,6 +1,73 @@
 <template>
     <div class="history">
+
         <div class="vertical-line">
+
+            <div class="wrap-cont">
+                <div class="year-cir">
+                    <div class="year">1946</div>
+                    <div class="cir"></div>
+                </div>
+
+                <div class="text">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati tempora vitae, architecto ad
+                    adipisci ut nobis? Alias aliquam vero et similique repudiandae, eum nulla impedit dicta id,
+                    exercitationem voluptatibus amet!
+                </div>
+            </div>
+
+            <div class="wrap-cont">
+                <div class="year-cir">
+                    <div class="year">1946</div>
+                    <div class="cir"></div>
+                </div>
+
+                <div class="text">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati tempora vitae, architecto ad
+                    adipisci ut nobis? Alias aliquam vero et similique repudiandae, eum nulla impedit dicta id,
+                    exercitationem voluptatibus amet!
+                </div>
+            </div>
+
+            <div class="wrap-cont">
+                <div class="year-cir">
+                    <div class="year">1946</div>
+                    <div class="cir"></div>
+                </div>
+
+                <div class="text">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati tempora vitae, architecto ad
+                    adipisci ut nobis? Alias aliquam vero et similique repudiandae, eum nulla impedit dicta id,
+                    exercitationem voluptatibus amet!
+                </div>
+            </div>
+
+            <div class="wrap-cont">
+                <div class="year-cir">
+                    <div class="year">1946</div>
+                    <div class="cir"></div>
+                </div>
+
+                <div class="text">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati tempora vitae, architecto ad
+                    adipisci ut nobis? Alias aliquam vero et similique repudiandae, eum nulla impedit dicta id,
+                    exercitationem voluptatibus amet!
+                </div>
+            </div>
+
+
+            <div class="wrap-cont">
+                <div class="year-cir">
+                    <div class="year">1946</div>
+                    <div class="cir"></div>
+                </div>
+
+                <div class="text">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati tempora vitae, architecto ad
+                    adipisci ut nobis? Alias aliquam vero et similique repudiandae, eum nulla impedit dicta id,
+                    exercitationem voluptatibus amet!
+                </div>
+            </div>
 
         </div>
 
@@ -12,21 +79,72 @@
 </script>
 
 <style>
+.wrap-cont {
+    width: 100vw;
+    height: auto;
+    height: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: end;
+    gap: 10px;
+}
+
+.text {
+    width: 81%;
+    display: flex;
+    align-items: center;
+    position: relative;
+    color: white;
+    font-weight: normal;
+    font-size: 20px;
+    font-family: 'Nunito', sans-serif;
+}
+
+.year-cir {
+    width: 90px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: -10px;
+    transform: translateX(9px);
+}
+
+.year {
+    color: white;
+    font-size: 22px;
+    font-weight: bold;
+}
+
 .history {
     width: 79%;
     margin-left: 10%;
     margin-right: auto;
     display: flex;
-    flex-direction: column;
-    align-items: end;
-    background-color: aqua;
+    flex-direction: row;
+    justify-content: end;
+    align-items: start;
 }
 
 
 .vertical-line {
-    height: 653px;
-    margin-bottom: 20px;
-    border-left: 6px solid white;
+    height: 700px;
+    width: 2px;
+    margin-bottom: 100px;
+    border-radius: 30px 30px 30px 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: end;
+    gap: 120px;
+    background-color: white;
 }
 
+.cir {
+    width: 20px;
+    height: 20px;
+    border-radius: 100%;
+    background-color: white;
+    position: relative;
+}
 </style>
